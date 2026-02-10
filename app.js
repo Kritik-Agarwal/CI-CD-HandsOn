bc66756c57ff1cf5588ada74999e77cb7054af04
Kritik Agarwal <kritik.agarwal@kickdrumtech.com>
2026-02-10 17:52:23 +0530

    commit with code

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Node.js Pipeline is successfully deploying to AWS!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
